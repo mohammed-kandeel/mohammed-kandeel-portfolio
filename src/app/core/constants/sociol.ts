0a1d4d7481a18cce2522dd4d1ac48aa5823d1b82
mohammed-kandeel <kandeelmohamed044@gmail.com>
2026-05-27 03:58:12 +0300

    cv url

--- src/app/core/constants/sociol.ts
+++ src/app/core/constants/sociol.ts
@@ -4,5 +4,5 @@ export const sociol = {
   whatsApp: 'https://wa.me/+201016676719',
   gitHub: 'https://github.com/mohammed-kandeel',
   linkedIn: 'https://www.linkedin.com/in/mohammed--kandeel',
-  cv: '',
+  cv: 'https://drive.google.com/file/d/1JuVdPktBNVuwrBxMsx8OdTW6XNE9zCwJ/view?usp=sharing',
 };
